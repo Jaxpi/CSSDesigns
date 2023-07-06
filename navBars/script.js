d6c86d48b82514806918b9a8a7b690709b8cbb7e
Jax I <jaxpi85@gmail.com>
2023-07-06 09:13:09 -0400

    added home page to allow for multiple element pages and changed e.target to id for greater amount of targets, but so far have to reuse id for each element

---
+++ navBars/script.js
@@ -0,0 +1,25 @@
+const navGroup1 = document.getElementById("nav-group1"); 
+let prevButton1 = null;
+const buttonPressed1 = (e) => {
+  if (e.target.id === "nav-group1") {
+    e.target.classList.add('active1'); 
+    if(prevButton1 !== null) {
+      prevButton1.classList.remove('active1');  
+    }
+    prevButton1 = e.target;
+  }
+}
+navGroup1.addEventListener("click", buttonPressed1);
+
+const navGroup2 = document.getElementById("nav-group2"); 
+let prevButton2 = null;
+const buttonPressed2 = (e) => {
+    if (e.target.id === "nav-group2") {
+    e.target.classList.add('active2'); 
+    if(prevButton2 !== null) {
+      prevButton2.classList.remove('active2');  
+    }
+    prevButton2 = e.target;
+  }
+}
+navGroup2.addEventListener("click", buttonPressed2);
