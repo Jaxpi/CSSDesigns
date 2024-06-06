b8b0843755a7385fa352395e3d9d2028ed321703
Jax I <jaxpi85@gmail.com>
2024-06-06 10:56:14 -0400

    updated code for form 3 to include a pop-up for signing up, working on styling and simplifying code for js actions

--- signUpForms/formsScript.js
+++ signUpForms/formsScript.js
@@ -17,8 +17,19 @@ function openForm2() {
     document.getElementById("myForm2").style.display = "block";
   }
 }
-  
-  
-  function closeForm2() {
-    document.getElementById("myForm2").style.display = "none";
+
+function closeForm2() {
+  document.getElementById("myForm2").style.display = "none";
+}
+
+function openForm3() {
+  if (document.getElementById("myForm3").style.display == "block") {
+    document.getElementById("myForm3").style.display = "none";
+  } else {
+    document.getElementById("myForm3").style.display = "block";
   }
+}
+
+function closeForm3() {
+  document.getElementById("myForm3").style.display = "none";
+}
