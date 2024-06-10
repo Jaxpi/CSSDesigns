78535eaa1dc78676fa13b0f079d6551bf1fec88f
Jax I <jaxpi85@gmail.com>
2024-06-10 16:34:18 -0400

    debugged notes reverting to style1 by rearranging the global calls

--- noteCards/noteCardsScript.js
+++ noteCards/noteCardsScript.js
@@ -4,7 +4,6 @@ let notes = document.querySelectorAll(".input-box");
 
 // Get references to the relevant elements
 const styleToggle = document.getElementById("style-toggle");
-// const inputBoxImg = document.querySelectorAll(".input-box-img");
 const container = document.getElementById("container");
 const containerH1 = document.getElementById("container-h1");
 const containerH1Img = document.getElementById("container-h1-img");
@@ -12,11 +11,10 @@ const containerBtn = document.getElementById("container-button");
 const containerBtnImg = document.getElementById("container-button-img");
 
 // Initialize with the default style and set new style upon click event
-let currentStyle = parseInt(localStorage.getItem("currentStyle")) || 1;
+let currentStyle = parseInt(localStorage.getItem("currentStyle")) || 3;
 
 const elementsToUpdate = [
   notesContainer.getElementsByTagName("p"),
-  // inputBoxImg,
   container,
   containerH1,
   containerH1Img,
@@ -26,20 +24,16 @@ const elementsToUpdate = [
 
 function applyStyle(style) {
   elementsToUpdate.forEach((element) => {
-    if (element.length > 0) {
-      // If it is, iterate over each node
-      for (i = 0; i < element.length;i++){
+    if (element.length >= 0) {
+      for (i = 0; i < element.length; i++) {
         element[i].classList.remove("style1", "style2", "style3");
         element[i].classList.add(`style${style}`);
-      };
+      }
     } else {
-      // If it's a single element, apply the class name directly
       element.className = `style${style}`;
     }
   });
 }
-
-applyStyle(currentStyle);
 
 styleToggle.addEventListener("click", () => {
   currentStyle = (currentStyle % 3) + 1;
@@ -58,7 +52,7 @@ function updateStorage() {
 createBtn.addEventListener("click", () => {
   let inputBox = document.createElement("p");
   let img = document.createElement("img");
-  inputBox.className = "input-box style"+currentStyle;
+  inputBox.className = "input-box style" + currentStyle;
   inputBox.setAttribute("contenteditable", "true");
   img.src = "../assets/deleteIcon.png";
   img.className = "input-box-img";
@@ -87,3 +81,4 @@ document.addEventListener("keydown", (event) => {
 });
 
 showNotes();
+applyStyle(currentStyle);
